55b2c2145daee167892749abe363899f5f3382be
Denis Markman <dan.markman@gmail.com>
2014-12-21 22:05:48 +0200

    Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,15 @@
+##Coursera course: R Programing
+##Week 3: Programming Assignment2: Lexical Scoping
+##Date of submission 2014-12-21
+
+
+##Function makeCacheMatrix
+##Creates the list of functions for working with cache for inversed matrix:
+##1. For setting the value of the matrix
+##2. For getting the value of the matrix
+##3. For setting the value of the inversed matrix
+##4. For getting the value of the inversed matrix
+
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -13,8 +25,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+##Function cacheSolve
+##Checks if there's an inverse matrix already
+##If no - creates the inversed matrix and sets it's value to the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
   if(!is.null(m)) {
     message("getting cached data")
